c423f66c30a65bb2ca9ae273221595542bd4eccd
Josh Gachnang <josh@pcsforeducation.com>
2014-03-19 16:47:38 -0700

    Adding swift temp url support
    This patch will allow properly configured Glance servers to return a temporary
    URL for an object hosted on Swift. It will require Glance to use Swift
    as its backend. A temporary URL will let the agent download an image from
    Glance without requiring an auth_token, which gives access more than just
    Glance. The easiest way to use it is to enable direct_url in Glance, but
    you can set the appropriate config options to avoid needing to enable it.
    
    We/I need to add a note in the docs about Swift being a possible dependency
    for Ironic deploys using IPA, along with how to set the Temp URL key.
    
    Swift perfomance concerns will be addressed in this blueprint:
    https://blueprints.launchpad.net/ironic/+spec/improve-swift-agent-downloads
    
    SecurityImpact
    DocImpact
    
    Implements: blueprint swift-temp-urls
    Change-Id: I10e4784eee63e8edc9ba30a9c5004a08aa3a6d8e
    (cherry picked from commit 327d32726dbc1d9a1ee97315a66210f76c8123f8)

--- ironic/common/glance_service/v2/image_service.py
+++ ironic/common/glance_service/v2/image_service.py
@@ -13,12 +13,18 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import hashlib
+import hmac
+import time
+
 from oslo.config import cfg
+import six.moves.urllib.parse as urlparse
 
 from ironic.common import exception as exc
 from ironic.common.glance_service import base_image_service
 from ironic.common.glance_service import service
 from ironic.common.glance_service import service_utils
+from ironic.common import utils
 
 
 glance_opts = [
@@ -26,7 +32,36 @@ glance_opts = [
                 default=[],
                 help='A list of URL schemes that can be downloaded directly '
                 'via the direct_url.  Currently supported schemes: '
-                '[file].')
+                '[file].'),
+    # To upload this key to Swift:
+    # swift post -m Temp-Url-Key:correcthorsebatterystaple
+    cfg.StrOpt('swift_temp_url_key',
+                help='The secret token given to Swift to allow temporary URL '
+                     'downloads.'),
+    cfg.ListOpt('swift_temp_url_methods',
+                default=['GET'],
+                help='A list of HTTP methods allowed for the temporary URL in '
+                     'Swift. Must be one of ["GET", "HEAD", "PUT", "POST", '
+                     '"DELETE"].'),
+    cfg.IntOpt('swift_temp_url_duration',
+                default=3600,
+                help='The length of time in seconds that the temporary URL '
+                     'will be valid for.'),
+    cfg.StrOpt('swift_scheme',
+                help='Override the scheme for the Swift server. Should be '
+                     'either "http" or "https".'),
+    cfg.StrOpt('swift_endpoint_url',
+                help='Override the base URL for the Swift server (without '
+                     'scheme). Should not have a trailing "/" or API version. '
+                     'For example, use "swift.example.com".'),
+    cfg.StrOpt('swift_path',
+                help='Override the the string between host and Swift '
+                     'container name, including version string. Should not '
+                     'include leading or trailing slashes. For example, '
+                     '"v1/TENANT_NAME_TENANT_ID".'),
+    cfg.StrOpt('swift_backend_container',
+                help='Override the name of the Swift container in which '
+                     'Glance stores its images.')
 ]
 
 CONF = cfg.CONF
@@ -56,6 +91,140 @@ class GlanceImageService(base_image_service.BaseImageService,
 
     def delete(self, image_id):
         return self._delete(image_id, method='delete')
+
+    def swift_temp_url(self, image_info, duration=None):
+        """Generate a no-auth Swift temporary URL.
+
+        Returns a temporary URL that is good for duration seconds. This
+        allows Ironic to download a Glance image without passing around an
+        auth_token. If Glance has 'show_image_direct_url' enabled, we will
+        use the URL from that to find the Swift URL. Otherwise, we will infer
+        the URL from the Glance image-id and require a set of config options
+        to be set: 'swift_scheme', 'swift_endpoint_url', 'swift_path',
+        'swift_backend_container'.
+
+        :param image_info: The return from a GET request to Glance for a
+        certain image_id. Should be a dictionary, with keys like 'name' and
+        'checksum'. See
+        http://docs.openstack.org/developer/glance/glanceapi.html for
+        examples.
+        :returns: A signed Swift URL that can be downloaded from without auth.
+
+        :raises: ImageNotFound, InvalidParameterValue
+        """
+        no_direct_url_required_params = [
+            'swift_scheme', 'swift_endpoint_url', 'swift_path',
+            'swift_backend_container']
+
+        self._validate_temp_url_config()
+        duration = duration or CONF.glance.swift_temp_url_duration
+        if duration is None:
+            raise exc.InvalidParameterValue(_('Must either call with a '
+                                              'duration or set the config '
+                                              'option '
+                                              'swift_temp_url_duration'))
+
+        direct_url = image_info['properties'].get('direct_url')
+        if not direct_url:
+            # If not direct_url, we can still get the Swift url if enough
+            # static config settings are set. We'll just use image_id as the
+            # object_id
+            for param in no_direct_url_required_params:
+                if not getattr(CONF.glance, param):
+
+                    raise exc.InvalidMAC(_(
+                        'Images must either have a direct_url or each of %s '
+                        'to use swift temp urls.') %
+                        no_direct_url_required_params)
+            url_fragments = {
+                'host': CONF.glance.swift_endpoint_url,
+                'scheme': CONF.glance.swift_scheme,
+                'path': CONF.glance.swift_path,
+                'container': CONF.glance.swift_backend_container,
+                'object_id': image_info['properties'].get('image_id')
+            }
+
+        else:
+            url_fragments = self._swift_url_fragments(direct_url)
+
+        template = '/{path}/{container}/{object_id}'
+        url_path = template.format(
+            path=url_fragments['path'].lstrip('/').rstrip('/'),
+            container=url_fragments['container'],
+            object_id=url_fragments['object_id']
+        )
+
+        expiration = int(time.time() + duration)
+        # Make a normalized space delimited list of upper case version of
+        # provided methods.
+        methods = ' '.join(map(lambda x: x.upper(),
+                               CONF.glance.swift_temp_url_methods))
+        hmac_body = '\n'.join([methods, str(expiration), url_path])
+        key = CONF.glance.swift_temp_url_key
+        # Encode to UTF-8
+        try:
+            sig = hmac.new(key.encode(),
+                           hmac_body.encode(),
+                           hashlib.sha1).hexdigest()
+        except UnicodeDecodeError:
+            raise exc.InvalidParameterValue(_('Could not convert '
+                                              'swift temp url arguments '
+                                              'to Unicode for url.'))
+
+        return ('{scheme}://{host}{url_path}?temp_url_sig='
+                '{sig}&temp_url_expires={exp}'.format(
+                    scheme=url_fragments['scheme'],
+                    host=url_fragments['host'],
+                    url_path=url_path,
+                    sig=sig,
+                    exp=expiration)
+                )
+
+    def _validate_temp_url_config(self):
+        """Validate all the settings for a temp url."""
+        if not CONF.glance.swift_temp_url_key:
+            raise exc.InvalidParameterValue(_(
+                'Swift temporary URLs require a shared secret to be created. '
+                'You must provide swift_temp_url_key as a config option.'))
+        # Normalize
+        methods = map(lambda x: x.upper(), CONF.glance.swift_temp_url_methods)
+        if not methods:
+            raise exc.InvalidParameterValue(_('Must have at least one HTTP '
+                                              'method.'))
+
+        valid_methods = ['GET', 'HEAD', 'PUT', 'POST', 'DELETE']
+        if len(set(methods) - set(valid_methods)) > 0:
+            raise exc.InvalidParameterValue(
+                _('Parameter '"swift_temp_url_methods"' must be set to one or '
+                  'more of these methods:  %s') % valid_methods)
+
+    def _swift_url_fragments(self, direct_url):
+        """Get the temp URL fragments from thge direct_url."""
+        parsed = urlparse.urlparse(direct_url)
+
+        # Remove username/password if they exist.
+        swift_host = parsed.netloc.split('@')[-1]
+
+        try:
+            empty, path, container, object_id = parsed.path.split('/')
+        except ValueError:
+            raise exc.ImageUnacceptable(_(
+                'The Glance direct_url %s could not be decoded in "path", '
+                '"container" and "object_id" variables. You may need to set '
+                'the Swift config options to customize how the Swift temporary'
+                ' URLs are built.') % direct_url)
+        if not utils.is_uuid_like(object_id):
+            raise exc.ImageUnacceptable(_('The object name %s in the Glance '
+                                          'direct_url isn\'t a valid UUID.') %
+                                          object_id)
+
+        return {
+            'host': CONF.glance.swift_endpoint_url or swift_host,
+            'scheme': CONF.glance.swift_scheme or parsed.scheme,
+            'path': CONF.glance.swift_path or path,
+            'container': CONF.glance.swift_backend_container or container,
+            'object_id': object_id,
+        }
 
     def _get_location(self, image_id):
         """Returns the direct url representing the backend storage location,
